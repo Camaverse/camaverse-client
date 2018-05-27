1cdd0a9b8231a8c0cf3be95cfd008db5987fd81c
larrysharpe <larry.l.sharpe@gmail.com>
2018-05-27 15:44:28 -0400

    updated api path

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_PATH: '"http://localhost:3010/"',
+  API_PATH: '"http://localhost:3010/1.0/"',
   ASSETS_PATH: '"http://localhost:3015/"',
   SOCKET_PATH: '"http://localhost:3010/"'
 })
