443ed0a47439448a72134319a0ffd10726a8272d
larrysharpe <larry.l.sharpe@gmail.com>
2018-05-27 01:47:23 -0400

    New header

--- src/components/HeaderGlobal.vue
+++ src/components/HeaderGlobal.vue
@@ -1,38 +1,11 @@
 <template>
   <header>
     <nav class="nav-primary navbar">
-
       <router-link to="/">CWL</router-link>
-
-      <div v-if="!isLoggedIn">
-        <a href="#" v-b-modal.signupModal ref="signupModal" id="signupModal">Join For Free</a> |
-        <a href="#" v-b-modal.loginModal ref="loginModal" id="loginModal">Login</a>
-      </div>
-
-      <div v-if="user.username">
-        Hi {{user.username}}
-
-        <b-button variant="warning" id="btn-coins" @click.prevent="coinsClick()">
-          Coins
-          <b-badge variant="light">
-            {{user.coins ? user.coins.balance : 0}}
-          </b-badge>
-        </b-button>
-
-        <pop-over v-if="isLoggedIn" target="btn-coins" placement="bottom" title="Get More Coins" id="popover-get-more-coins-1"></pop-over>
-
-<!--
-        <b-button variant="info">
-          XP
-          <b-badge variant="light">
-            {{user.xp || 0}}
-          </b-badge>
-        </b-button>
--->
-      </div>
-      <div v-if="isLoggedIn">
-        <a href="#" @click.prevent="logout">Logout</a>
-      </div>
+      <guest-header-links></guest-header-links>
+      <header-info-guest></header-info-guest>
+      <header-info-user></header-info-user>
+      <logout-link></logout-link>
     </nav>
   </header>
 </template>
@@ -51,64 +24,17 @@
   }
 </style>
 <script>
-  import {mapGetters} from 'vuex'
-  import PopOver from './MainCoins.popover'
+  import GuestHeaderLinks from './GuestHeaderLinks'
+  import HeaderInfoGuest from './HeaderInfoGuest'
+  import HeaderInfoUser from './HeaderInfoUser'
+  import LogoutLink from './LogoutLink'
 
   export default {
-    methods: {
-      coinsClick () {
-        if (!this.isLoggedIn) {
-          console.log('open signupModal')
-          this.$root.$emit('bv::show::popover', 'signupModal')
-        }
-      },
-      leaveRoom () {
-        if (this.currentRoom) {
-          let room = this.currentRoom._id
-          let user = this.user.slug
-          let data = {room, user}
-          this.$socket.emit('leaveRoom', data)
-        }
-      },
-      logout: function () {
-        let url = process.env.API_PATH + '/logout/' + this.user.username
-        this.$http.get(url).then((res) => {
-          if (res.data.success) {
-            this.leaveRoom()
-            this.unsetUser()
-            this.init()
-            this.$router.push('/')
-          }
-        })
-      },
-      unsetUser () {
-        this.$store.commit('resetUser')
-        this.$localStorage.remove('user')
-      },
-      handleConnectGuest (err, guest) {
-        if (err) console.log(err)
-        else this.setUser(guest)
-      },
-      init () {
-        let usr = this.$localStorage.get('user')
-        if (usr) this.setUser(JSON.parse(usr))
-        else this.$socket.emit('connect-guest', this.handleConnectGuest)
-      },
-      setUser (user) {
-        let userString = JSON.stringify(user)
-        this.$store.commit('setUser', user)
-        this.$localStorage.set('user', userString)
-      }
-    },
-    computed: {
-      ...mapGetters([
-        'isLoggedIn',
-        'user',
-        'currentRoom'
-      ])
-    },
     components: {
-      PopOver
+      GuestHeaderLinks,
+      HeaderInfoGuest,
+      HeaderInfoUser,
+      LogoutLink
     }
   }
 </script>
