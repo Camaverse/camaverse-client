fd9aa57a22cb03193cf950b586a9b6fe17a78a9a
larrysharpe <larry.l.sharpe@gmail.com>
2018-06-19 08:25:57 -0400

    refactored coin store

--- src/stores/coins.store.js
+++ src/stores/coins.store.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 export const coins = {
   namespaced: true,
   state: {
-    balance: 0
+    balance: 0,
+    tip: {
+      isPrivate: false,
+      msg: {
+        isPrivate: false,
+        content: ''
+      }
+    }
   },
   getters: {
     balance: state => state.balance
@@ -40,6 +47,50 @@ export const coins = {
         } else {
           commit('updateCoins', data.coins.balance)
         }
+      })
+    },
+    sendTip ({commit, dispatch}, amount) {
+      return new Promise((resolve, reject) => {
+        dispatch('createTip', amount)
+        .then((tip) => {
+          this._vm.$socket.emit('sendTip', tip, ({errors, balance}) => {
+            if (errors) reject(new Error(errors))
+            else {
+              commit('updateCoins', balance)
+              resolve({balance})
+            }
+          })
+        })
+      })
+    },
+    createTip ({rootGetters, state}, amount) {
+      const room = rootGetters['chat/currentRoom']
+      const user = rootGetters['user']
+      const tip = state.tip
+
+      const ret = {
+        amount,
+        isOffline: (!room),
+        isPrivate: tip.isPrivate,
+        msg: {
+          isPrivate: tip.msg.isPrivate,
+          content: tip.msg.content
+        },
+        from: {
+          slug: user.slug,
+          username: user.username
+        },
+        to: {
+          id: (room) ? room.broadcasterID : null,
+          show: (room) ? room.show : null,
+          slug: (room) ? room.slug : null,
+          room: (room) ? room._id : null
+        },
+        clientTime: Date.now()
+      }
+
+      return new Promise((resolve, reject) => {
+        resolve(ret)
       })
     }
   }
