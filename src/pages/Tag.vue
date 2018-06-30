6bc212e41bda5bfd8694c9e4503ec968e7f7759c
larrysharpe <larry.l.sharpe@gmail.com>
2018-06-30 10:06:50 -0400

    create a tag results page

---
+++ src/pages/Tag.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-lists">
+    <broadcaster-grid :tags="$route.params.tag"></broadcaster-grid>
+  </div>
+</template>
+<script>
+  import BroadcasterGrid from '../components/BroadcasterGrid/BroadcasterGrid.vue'
+
+  export default {
+    components: {
+      BroadcasterGrid
+    }
+  }
+</script>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import APITest from '@/pages/APITest'
 import Home from '@/pages/Home'
+import Tag from '@/pages/Tag'
 import Watch from '@/pages/Watch'
 import NotFound from '@/pages/404'
 
@@ -12,6 +13,7 @@ export default new Router({
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/apitest', name: 'APITest', component: APITest },
+    { path: '/:tag', name: 'Tag', component: Tag },
     { path: '/watch/:slug', name: 'Watch', component: Watch },
     // catch all
     { path: '*', name: 'Not Found', component: NotFound }
