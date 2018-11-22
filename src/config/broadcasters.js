9ff6a843e001d6fba2cc608d20ccdf4ad5839617
Larry Sharpe <larry.l.sharpe@gmail.com>
2018-11-22 00:52:32 -0500

    broadcaster config seed

--- src/config/broadcasters.js
+++ src/config/broadcasters.js
@@ -1,12 +1,15 @@
 let broadcasters = [];
-import tagList from './tags'
+import showList from './shows';
+import tagList from './tags';
 
 for (let i = 1; i <= 100; i++){
     let _tmp = {
-        username: `Username ${i}`,
+        gender: '',
+        show: '',
         slug: `username-${i}`,
         tags: [tagList[Math.floor(Math.random()*tagList.length)]],
-        topic: `here is my topic ${i}`
+        topic: `here is my topic ${i}`,
+        username: `Username ${i}`
     }
 
     broadcasters.push(_tmp);
