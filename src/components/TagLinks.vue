1776467ca03522a2cb8ceb3ce2964a28fb0eed1a
Larry Sharpe <larry.l.sharpe@gmail.com>
2018-11-23 15:47:59 -0500

    Created Tag Links Component

---
+++ src/components/TagLinks.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+b-navbar-nav.tag-links
+    b-nav-item.pr-2(:to='`/tagged/${tag}`', v-for='(tag, index) in tags', :key='index') \#{{tag}}
+</template>
+<script>
+export default {
+    name: 'tag-links',
+    props: ['tags']
+}
+</script>
+<style lang="scss">
+.tag-links {
+    font-size: .75em;
+    .nav-link {
+        padding: 0
+    }
+}
+</style>
