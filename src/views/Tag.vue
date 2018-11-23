3942cda76f3716118511584b52ca895c07c9133d
Larry Sharpe <larry.l.sharpe@gmail.com>
2018-11-23 09:36:20 -0500

    Add capitalize to Vue Tags

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     .page-tags
-        h2 Live {{$route.params.tag}} Web Cams
+        h2 Live {{$route.params.tag | capitalize }} Web Cams
         div(v-if="items.length")
             broadcaster-grid(:items="items")
         .tag-grid(v-else)
@@ -16,6 +16,13 @@
         data () {
             return {
                 items: broadcasters.broadcasters.filter(b => b.tags.indexOf(this.$route.params.tag) > -1)
+            }
+        },
+        filters: {
+            capitalize: function (value) {
+                if (!value) return ''
+                value = value.toString()
+                return value.charAt(0).toUpperCase() + value.slice(1)
             }
         }
     }
