799a9776e485138e3c3e3c83d5660aa1a56529a9
Larry Sharpe <larry.l.sharpe@gmail.com>
2018-11-23 12:44:07 -0500

    updated the broadcasters config

--- src/config/broadcasters.js
+++ src/config/broadcasters.js
@@ -1,18 +1,26 @@
 let broadcasters = [];
+import genderList from './genders';
 import showList from './shows';
 import tagList from './tags';
 
+Array.prototype.getRandom = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
 for (let i = 1; i <= 100; i++){
     let _tmp = {
-        gender: '',
-        show: '',
+        gender: genderList.getRandom(),
+        show: showList.getRandom(),
         slug: `username-${i}`,
-        tags: [tagList[Math.floor(Math.random()*tagList.length)]],
+        tags: [tagList.getRandom()],
         topic: `here is my topic ${i}`,
         username: `Username ${i}`
     }
 
     broadcasters.push(_tmp);
 }
+
+console.log(broadcasters)
+
 
 export default { broadcasters }
