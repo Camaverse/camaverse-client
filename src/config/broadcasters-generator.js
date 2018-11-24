d844fe2c0ccc799affb287d62d5ea0757e4dd3e4
Larry Sharpe <larry.l.sharpe@gmail.com>
2018-11-24 02:04:59 -0500

    created broadcaster generator

---
+++ src/config/broadcasters-generator.js
@@ -0,0 +1,43 @@
+let broadcasters = [];
+import genderList from './genders';
+import showList from './shows';
+import tagList from './tags';
+
+const rando = () => Math.floor(Math.random() * 19) + 1
+const safeForWork = false;
+
+Array.prototype.getRandom = function (amt = null) {
+    let _tmpArr = this.join("|").split("|")
+    let ret = [];
+    for (let i = 0; i < Math.max(amt,1); i++){
+        let index = Math.floor(Math.random() * _tmpArr.length);
+        ret.push(_tmpArr[index])
+        _tmpArr.splice(index, 1);
+    }
+
+    if (amt === null) return ret[0];
+    return ret;
+}
+
+for (let i = 1; i <= 100; i++){
+
+    const gender = genderList.getRandom();
+    const imgSite = gender === 'm' ? 'hunkholder' : 'babeholder';
+
+    let _tmp = {
+        gender: gender,
+        show: showList.getRandom(),
+        slug: `username-${i}`,
+        tags: tagList.getRandom(4),
+        thumb: `http://${imgSite}.pixoil.com/img/300/100/?${i}`,
+        topic: `here is my topic ${i}`,
+        username: `Username ${i}`
+    }
+
+    broadcasters.push(_tmp);
+}
+
+console.log(broadcasters)
+
+export default { broadcasters }
+
