d8231239a2927dec711cedb14fbe3129d8fe5c2a
lsharpe <larry.l.sharpe@gmail.com>
2019-12-22 09:05:38 -0600

    adjust main container padding

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
 </script>
 <style lang="scss">
     main {
-        padding: 35px 10px 58px 10px;
+        padding: 65px 10px 58px 10px;
     }
     .flex {
         display: flex;
