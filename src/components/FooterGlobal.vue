0d0708accc97f8c0ba5db1660fc47e2c99d2acb0
lsharpe <larry.l.sharpe@gmail.com>
2019-12-22 16:07:17 -0600

    open footer links in a new window

--- src/components/FooterGlobal.vue
+++ src/components/FooterGlobal.vue
@@ -9,36 +9,36 @@
             .row.block-links
                 .col-6.col-md-3
                     | Support
-                    a(href='') Billing
-                    a(href='') Contact Us
-                    a(href='') Feedback
+                    a(href="", target="_blank") Billing
+                    a(href="", target="_blank") Contact Us
+                    a(href="", target="_blank") Feedback
                 .col-6.col-md-3
                     | Partners
-                    a(href='') Make Money
-                    a(href='') Streamers
-                    a(href='') Studios
-                    a(href='') Affiliates
-                    a(href='') Buy Traffic
-                    a(href='') Contributors
+                    a(href="", target="_blank") Make Money
+                    a(href="", target="_blank") Streamers
+                    a(href="", target="_blank") Studios
+                    a(href="", target="_blank") Affiliates
+                    a(href="", target="_blank") Buy Traffic
+                    a(href="", target="_blank") Contributors
                 .col-md-3.col-sm-6
                     | Business
-                    a(href='') Events
-                    a(href='') Media
-                    a(href='') Jobs
+                    a(href="", target="_blank") Events
+                    a(href="", target="_blank") Media
+                    a(href="", target="_blank") Jobs
                 .col-md-3.col-sm-6
                     | Legal
-                    a(href='') Law Enforcement
-                    a(href='') Terms & Conditions
-                    a(href='') Privacy Policy
-                    a(href='') Cookies
-                    a(href='') Anti-Spam
-                    a(href='') Refunds
+                    a(href="", target="_blank") Law Enforcement
+                    a(href="", target="_blank") Terms & Conditions
+                    a(href="", target="_blank") Privacy Policy
+                    a(href="", target="_blank") Cookies
+                    a(href="", target="_blank") Anti-Spam
+                    a(href="", target="_blank") Refunds
 
             .footer-legal.text-center
                 p Main Office: Virginia, USA
                 p This site contains sexually explicit material. Enter ONLY if you are at least 18 years old and agree to our cookie rules.
                 p
-                    a.text-light(href='') 18 U.S.C. 2257 Record Keeping Requirements Compliance Statement
+                    a.text-light(href="", target="_blank") 18 U.S.C. 2257 Record Keeping Requirements Compliance Statement
                 p &copy; 2019 - {{new Date().getFullYear()}} Camaverse.com. All Rights Reseerved
 </template>
 <style>
