fa65ca6547237e52a6e25ab1b6fc93c1df18c8bc
lsharpe <larry.l.sharpe@gmail.com>
2019-12-29 18:38:32 -0500

    use dynamic mock data on watch page

--- src/views/Watch.vue
+++ src/views/Watch.vue
@@ -8,13 +8,31 @@
 </template>
 <script>
 import Broadcast from '@/components/Broadcast'
+import broadcasters from '@/mock/broadcasters'
 export default {
   name: 'watch',
   components: { Broadcast },
+  created () {
+    this.updateBroadcaster(this.$route.params.slug)
+  },
   data () {
     return {
-      username: 'Username Here',
-      topic: 'The topic goes here'
+      username: null,
+      slug: null,
+      topic: null
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.updateBroadcaster(to.params.slug)
+    next()
+  },
+  methods: {
+    updateBroadcaster (slug) {
+      this.slug = slug
+      const broadcaster = broadcasters.broadcasters.find(b => b.slug === this.slug)
+      Object.keys(broadcaster).forEach(key => {
+        this[key] = broadcaster[key]
+      })
     }
   }
 }
